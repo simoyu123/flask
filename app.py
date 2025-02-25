fd10e93f7912ca971a9b6d89dd9897b74273da3b
simoyu123 <lon24yu01@mpw.ac.uk>
2025-02-25 13:36:05 +0000

    get login

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = request.args.get("user")
-    if name == None:
+    username = request.args.get("username")
+    password = request.args.get("password")
+    if username == None:
         return render_template("index.html")
+    elif password == "123":
+        return "Hello " + username
     else:
-        return "Hello " + name
+        return "wrong password"
